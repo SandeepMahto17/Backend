3b9ca5dccf8b937be9592f79d54df97b4fdb9526
SandeepMahto17 <kumasandeep46@gmail.com>
2024-12-01 05:22:52 +0530

    cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: '*',
+  origin: 'https://front-chi-puce.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
